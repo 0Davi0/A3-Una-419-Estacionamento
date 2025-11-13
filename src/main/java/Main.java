c47cab8bd7dc72e4b948a128cea6bc1d51894757
Davi <davi.dias0431@gmail.com>
2025-11-13 15:28:22 -0300

    Opcionais implementados

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,26 +4,3 @@ public class Main {
         menu.mostraMenu();
     }
 }
-
-/*
-Coisas a Testar:
-    Registra o maximo de veiculo possiveis
-    verificar se pode adicionar veiculos a mais que vagas
-    verificar se pode adicionar um veiculo ja adicionado
-    Relatorio de vagas
-    relatorio de veiculos
-    pesquisar um veiculo no estacionamento
-    pesquiser um veiculo que nao esta no estacionamento
-    relatorio de faturamento
-    Registra saido do veiculo
-    tentar registrar saido do mesmo veiculo novamente
-    tentar registrar saido de um veiculo que nunca esteve no estacionamento
-    pesquisar veiculo que saiu do estacionamento
-    Relatorio de vagas
-    relatorio de veiculos
-    relatorio de faturamento
-    adicionar veiculo a fila
-    registrar saida do veiculo
-    ver se o veiculo da fila foi adicionado a fila
-    remover veiculo que estava na fila e foi para o estacionamneto
- */
